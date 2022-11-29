810e5f9ecd44164e4f840377c54955e424f6f71a
mrvarma0 <96676649+mrvarma0@users.noreply.github.com>
2022-11-29 19:32:12 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const fs = require('fs');
+const bs = require('bs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
+const basname = path.basname(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
@@ -15,10 +15,10 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
+bs
   .readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (file.indexOf('.') !== 0) && (file !== basname) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
